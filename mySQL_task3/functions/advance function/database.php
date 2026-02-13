84ab8601a4cc1fb7f0d9a96c4f067598484a8c3b
Zenthousand <147981416+Zenthousand@users.noreply.github.com>
2026-02-13 02:08:49 -0800

    Delete mySQL_task3 directory

--- mySQL_task3/functions/advance function/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "sql_demo");
-
-$result = $conn->query("SELECT DATABASE() AS db_name");
-$row = $result->fetch_assoc();
-
-echo "<h2>DATABASE()</h2>";
-echo "Current Database: " . $row['db_name'];
-?>
--- mySQL_task3/functions/advance function/version.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "sql_demo");
-
-$result = $conn->query("SELECT VERSION() AS version");
-$row = $result->fetch_assoc();
-
-echo "<h2>MySQL VERSION()</h2>";
-echo "Database Version: " . $row['version'];
-?>
